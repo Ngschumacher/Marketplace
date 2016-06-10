505deb65d35e72fa765c5d3763f6a34d71b5e391
Niclas Schumacher <Niclas@schu.dk>
2016-06-10 20:36:30 +0200

    Init

---
+++ MarketPlace.Services/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MarketPlace.Services")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MarketPlace.Services")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("461cf735-a7ab-44fb-935b-0b74ac7c1661")]
---
+++ Marketplace.Services1/Catalog/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core;
+using Nop.Core.Domain.Catalog;
+using Nop.Services.Catalog;
+
+namespace Marketplace.Services1.Catalog
+{
+	interface ICategoryService :  Nop.Services.Catalog.ICategoryService
+	{
+		IPagedList<ProductCategory> GetProductVendorCategoriesByCategoryId(int categoryId, int vendorId,
+		  int pageIndex = 0, int pageSize = int.MaxValue, bool showHidden = false);
+	}
+}
